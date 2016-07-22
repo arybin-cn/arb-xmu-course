78928b156457406c3fbe49a506d5fa5f679afd54
arybin <arybin@163.com>
2016-07-22 15:05:32 +0800

    require lib files recursively

--- lib/arb/xmu/course.rb
+++ lib/arb/xmu/course.rb
@@ -1,2 +1,5 @@
-require 'arb/xmu/course/models/course_entity'
-require 'arb/xmu/course/models/course_info'
+Gem::Specification.find_by_name('arb-xmu-course').full_require_paths.each do |dir|
+  Dir[dir+File::Separator+'**'+File::Separator+'*.rb'].each do |lib_name|
+    require lib_name
+  end
+end
