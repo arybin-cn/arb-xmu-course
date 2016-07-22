d1f2909355c75a765927e550da97247386ed7308
arybin <arybin@163.com>
2016-07-22 14:06:39 +0800

    started project

---
+++ lib/arb/xmu/course.rb
@@ -0,0 +1,9 @@
+require "arb/xmu/course/version"
+
+module Arb
+  module Xmu
+    module Course
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/arb/xmu/course/version.rb
@@ -0,0 +1,7 @@
+module Arb
+  module Xmu
+    module Course
+      VERSION = "0.1.0"
+    end
+  end
+end
